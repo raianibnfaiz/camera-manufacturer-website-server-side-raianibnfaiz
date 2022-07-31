6dac8f48e921a1bde5d78a9acc25b43f568d75be
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-07-31 11:35:17 +0600

    booking post api applied

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 require('dotenv').config();
 
@@ -8,6 +10,44 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djeaefi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected')
+        const cameraPartsCollection = client.db('manufacturer_database').collection('camera_parts');
+        const bookingCollection = client.db('manufacturer_database').collection('bookings');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = cameraPartsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await cameraPartsCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello !')
