dbf6adf0b4a2a32d1be547c1055bf1c7d5cd2ff2
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-08-02 13:48:28 +0600

    post api applied for adding new product to the database

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
             res.send(users);
         })
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const requester = req.decoded.email;
@@ -105,6 +112,11 @@ async function run() {
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
         })
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            const result = await cameraPartsCollection.insertOne(product);
+            res.send(result);
+        });
 
     }
     finally {
